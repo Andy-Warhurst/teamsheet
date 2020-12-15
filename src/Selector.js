c3a42ec01d69db58ff9f20d736b571f79ee2cc85
AndyWarhurst <41633910+AndyWarhurst@users.noreply.github.com>
2020-12-15 18:22:31 +1030

    Added Shirt Numbers

--- src/Selector.js
+++ src/Selector.js
@@ -1,6 +1,14 @@
 import React from "react";
 import Players from "./Players.json";
 
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import FormControl from "react-bootstrap/FormControl";
+import Table from 'react-bootstrap/Table';
+
+import InputGroup from "react-bootstrap/InputGroup";
+
 // const Rounds = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
 
 const Selector = ({team, guests, selected, dispatch}) => {
@@ -29,57 +37,35 @@ const Selector = ({team, guests, selected, dispatch}) => {
 
   }
 
+  function updateShirtNumber(player, number) {
+    console.log(player);
+    console.log(number);
+    player.shirtno = number;
+
+  }
+
+
+
   return (
     <div className="teamsheet-generator">
-    {/* <h3 >{team} (Round {round})</h3> */}
-      <h3 >{team}</h3>
-      <table>
-        <tbody>
-          <tr valign="top">
-            <td> 
-              <div >
-                <form>
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>Name</th>
-                        <th></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                    {
-                      availablePlayers.map(p => (
-                        <tr key={p.id}>
-                          <td>
-                            {p.name}
-                          </td>
-                          <td>
-                            <input 
-                            type="checkbox" checked={p.active}
-                            id={p.id} 
-                            onClick={() => updateSelected(p)}
-                            />
-              
-                          </td>
-                        </tr>
-                      ))
-                    }
 
-                    </tbody>
-                  </table>
-                </form>
-              </div> 
-            </td>
-            <td>
-              {/* <PrintPreview round={round} team={team} selected={selected}/> */}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    
+     <b>{team}</b>
 
-    
+      <Form>
 
+        {
+          availablePlayers.map(p => (
+            <Row>
+              <Col md={{ span: 8}}><Form.Check  id={"playing".concat(p.id)} label={p.name}
+                    onClick={() => updateSelected(p)}/> </Col>
+              <Col md={{ span: 4}}><FormControl  id={"shirtnumber".concat(p.id)}  name={"shirtnumber"} type="text" 
+                     onChange={(e) => updateShirtNumber(p,e.target.value)}/></Col>
+            </Row>
+          ))
+        }
+
+      </Form>
+     
     </div>
   );
 };
