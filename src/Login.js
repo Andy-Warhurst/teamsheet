16733fcf6fefbd94d9828f80d8684333bf7b47ec
Andy-Warhurst <andy@andywarhurst.co.uk>
2021-07-24 20:29:12 +0930

    Added Version

--- src/Login.js
+++ src/Login.js
@@ -7,8 +7,6 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Jumbotron from "react-bootstrap/Jumbotron";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Dropdown from "react-bootstrap/Dropdown";
 
 
 
@@ -31,50 +29,49 @@ const handleSubmit = (event) => {
 };
 
   return (
-  
+      <div>
+          <Jumbotron>
+              <h3>SAMSL Team Manager - Sign In</h3>
+              <Form onSubmit={handleSubmit}>
+                  <Form.Group as={Row} controlId="formHorizontalTeam" >
+                      <Form.Label column sm={2}>
+                          Team
 
-    <div>
-        <Jumbotron>
-            <h3>SAMSL Team Manager - Sign In</h3>
-    <Form onSubmit={handleSubmit}>
-        <Form.Group as={Row} controlId="formHorizontalTeam" >
-            <Form.Label column sm={2}>
-            Team
+                      </Form.Label>
+                      <Col sm={3}>
 
-            </Form.Label>
-            <Col sm={3}>
-            
-              <Form.Control 
-              as="select" 
-              onChange={(e) =>setTeamId(e.target.value)} 
-              
-              custom>
-                      {Teams.map((team) => (
-                        <option key={team.id} value={team.id}>{team.name}</option>
-                      ))}
-              </Form.Control>    
-            </Col>
-        </Form.Group>
+                          <Form.Control
+                              as="select"
+                              onChange={(e) =>setTeamId(e.target.value)}
 
-        <Form.Group as={Row} controlId="formHorizontalPassword">
-            <Form.Label column sm={2}>
-            Password
-            </Form.Label>
-            <Col sm={3}>
-            <Form.Control type="password" placeholder="Password" />
-            </Col>
-        </Form.Group>
+                              custom>
+                              {Teams.map((team) => (
+                                  <option key={team.id} value={team.id}>{team.name}</option>
+                              ))}
+                          </Form.Control>
+                      </Col>
+                  </Form.Group>
 
-        <Form.Group as={Row}>
-            <Col sm={{ span: 10, offset: 2 }}>
-            <Button 
-              type="submit"
-            >Sign in</Button>
-            </Col>
-        </Form.Group>
-        </Form>
-        </Jumbotron>
-    </div>
+                  <Form.Group as={Row} controlId="formHorizontalPassword">
+                      <Form.Label column sm={2}>
+                          Password
+                      </Form.Label>
+                      <Col sm={3}>
+                          <Form.Control type="password" placeholder="Password" />
+                      </Col>
+                  </Form.Group>
+
+                  <Form.Group as={Row}>
+                      <Col sm={{ span: 10, offset: 2 }}>
+                          <Button
+                              type="submit"
+                          >Sign in</Button>
+                      </Col>
+                  </Form.Group>
+              </Form>
+              <p><i>Version 1.0</i></p>
+          </Jumbotron>
+      </div>
   );
 };
 
