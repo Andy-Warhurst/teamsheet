5d208ff603b8f0c274030b1b7210fb96cee132ed
Andy-Warhurst <andy@andywarhurst.co.uk>
2021-08-19 15:49:49 +0930

    REvised Fixtures (19/08/21)

--- src/HomePage.js
+++ src/HomePage.js
@@ -23,7 +23,7 @@ const HomePage = (props) => {
   console.log("Props: ", props);
 
   const initialState = {
-    round: 17,
+    round: 18,
     // team: "Adelaide Spurs",
     team: Teams[props.teamId-1].name,
     selected: [],
