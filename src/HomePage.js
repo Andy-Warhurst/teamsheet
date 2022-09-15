bb85d243a22f3bfb5b06a55ff3704246c1b158f6
Andy-Warhurst <andy@andywarhurst.co.uk>
2022-09-15 12:44:24 +0930

    Set up for rond 18

--- src/HomePage.js
+++ src/HomePage.js
@@ -23,7 +23,7 @@ const HomePage = (props) => {
   console.log("Props: ", props);
 
   const initialState = {
-    round: "CUP-FINAL",
+    round: "18",
     // team: "Adelaide Spurs",
     team: Teams[props.teamId-1].name,
     selected: [],
