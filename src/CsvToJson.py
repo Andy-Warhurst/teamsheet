a71a8e33cbb9cec7705053aa207cb1aabc52c949
Andy-Warhurst <andy@andywarhurst.co.uk>
2024-04-04 17:25:46 +1030

    Updated to include new teams and uploaded latest players.

--- src/CsvToJson.py
+++ src/CsvToJson.py
@@ -3,7 +3,8 @@ import os
 
 df = pd.read_csv (r'/Users/Andy/Downloads/RegManagementDetails.csv')
 
-keeps=["FFA Number","Club Name","Firstname","Lastname","DOB","Product Name","Reg Status Secondary"]
+#keeps=["FFA Number","Club Name","Firstname","Lastname","DOB","Product Name","Reg Status Secondary"]
+keeps=["FFA Number","Firstname","Lastname","DOB","Product Name","Reg Status Secondary"]
 
 df=df[keeps]
 
@@ -18,14 +19,14 @@ del df['Lastname']
 df = df.replace({'Pulteney Old Scholars': 'Pulteney',
  'Plymptonians Old Boys': 'Old Plymptonians',
  'Mercedes Old Boys':'Mercedes',
- 'Cove Masters FC': 'Cove Masters',
- 'Cove Fusion': 'Cove FC',
- 'Southern Cross Masters': 'Southern Cross',
- 'Southern Sharks Masters':'Southern Sharks',
- 'Croatia Masters': 'Croatia',
- 'Sporting Masters': 'Sporting',
- 'Sturt-Marion (Masters)': 'Sturt Marion Thunder',
- 'Sturt-Marion B (Masters)': 'Sturt Marion Old Boys',
+ 'Cove FC Masters Player Registration': 'Cove FC',
+ #'Cove Fusion': 'Cove FC',
+ #'Southern Cross Masters': 'Southern Cross',
+ #'Southern Sharks Masters':'Southern Sharks',
+ #'Croatia Masters': 'Croatia',
+ #'Sporting Masters': 'Sporting',
+ #'Sturt-Marion (Masters)': 'Sturt Marion Thunder',
+ #'Sturt-Marion B (Masters)': 'Sturt Marion Old Boys',
  'Royal Ajvar Sports & Social Club': 'Royal Ajvar',
  'Austria Masters': 'Austria',
  'Adelaide Spurs Masters': 'Adelaide Spurs',
@@ -54,30 +55,28 @@ df = df.replace({'Product Name': r'.*Player Reg.*'},{'Product Name': 'Full'}, re
 
 df = df.replace({'Team Name': r'.*Adelaide Spurs.*'},{'Team Name': 'Adelaide Spurs'}, regex=True)
 df = df.replace({'Team Name': r'.*Austria.*'},{'Team Name': 'Austria'}, regex=True)
-df = df.replace({'Team Name': r'.*Cove.*'},{'Team Name': 'Cove FC'}, regex=True)
+df = df.replace({'Team Name': r'.*Cove.*'},{'Team Name': 'Cove Masters'}, regex=True)
 df = df.replace({'Team Name': r'.*Croatia.*'},{'Team Name': 'Croatia'}, regex=True)
-df = df.replace({'Team Name': r'.*Man City.*'},{'Team Name': 'Man City'}, regex=True)
-df = df.replace({'Team Name': r'.*Old Plympton.*'},{'Team Name': 'Old Plymptonians'}, regex=True)
+df = df.replace({'Team Name': r'.*Mercedes.*'},{'Team Name': 'Mercedes'}, regex=True)
 df = df.replace({'Team Name': r'.*Plympton FC.*'},{'Team Name': 'Plympton FC'}, regex=True)
+df = df.replace({'Team Name': r'.*Pulteney Crabs.*'},{'Team Name': 'Crabs'}, regex=True)
 df = df.replace({'Team Name': r'.*Pulteney.*'},{'Team Name': 'Pulteney'}, regex=True)
+df = df.replace({'Team Name': r'.*Crabs.*'},{'Team Name': 'Pulteney Crabs'}, regex=True)
 df = df.replace({'Team Name': r'.*Royal Ajvar.*'},{'Team Name': 'Royal Ajvar'}, regex=True)
 df = df.replace({'Team Name': r'.*Cross.*'},{'Team Name': 'Southern Cross'}, regex=True)
 df = df.replace({'Team Name': r'.*Sharks.*'},{'Team Name': 'Southern Sharks'}, regex=True)
 df = df.replace({'Team Name': r'.*Sporting.*'},{'Team Name': 'Sporting'}, regex=True)
-df = df.replace({'Team Name': r'.*St Peter\'s A.*'},{'Team Name': 'St Peters'}, regex=True)
-df = df.replace({'Team Name': r'.*St Peter\'s B.*'},{'Team Name': 'St Peters B'}, regex=True)
-df = df.replace({'Team Name': r'.*Thunder.*'},{'Team Name': 'Sturt Marion Thunder'}, regex=True)
-df = df.replace({'Team Name': r'.*Sturt-Marion Old.*'},{'Team Name': 'Sturt Marion Old Boys'}, regex=True)
+df = df.replace({'Team Name': r'.*St Peter\'s.*'},{'Team Name': 'St Peters'}, regex=True)
+df = df.replace({'Team Name': r'.*Fusion.*'},{'Team Name': 'Fusion'}, regex=True)
+df = df.replace({'Team Name': r'.*Sturt Marion.*'},{'Team Name': 'Sturt Marion OB'}, regex=True)
+df = df.replace({'Team Name': r'.*Fusion.*'},{'Team Name': 'Sturt Fusion'}, regex=True)
 df = df.replace({'Team Name': r'.*Orange.*'},{'Team Name': 'University Orange'}, regex=True)
 df = df.replace({'Team Name': r'.*Uni Old Boys.*'},{'Team Name': 'University Old Boys'}, regex=True)
-df = df.replace({'Team Name': r'.*West Beach \(Div 1\).*'},{'Team Name': 'West Beach'}, regex=True)
-df = df.replace({'Team Name': r'.*West Beach \(Div1\).*'},{'Team Name': 'West Beach'}, regex=True)
-#df = df.replace({'Team Name': r'.*West Beach (Div 1) Player Registration.*'},{'Team Name': 'West Beach'}, regex=True)
-df = df.replace({'Team Name': r'.*West Beach Old Boys.*'},{'Team Name': 'West Beach OB'}, regex=True)
-df = df.replace({'Team Name': r'.*Legends.*'},{'Team Name': 'West Beach Legends'}, regex=True)
+df = df.replace({'Team Name': r'.*West Beach.*'},{'Team Name': 'West Beach'}, regex=True)
+df = df.replace({'Team Name': r'.*Old Plympton.*'},{'Team Name': 'Old Plymptonians'}, regex=True)
 
 
-del df['Club Name']
+#del df['Club Name']
 
 df = df.rename(columns={"FFA Number": "id",
  "Team Name": "team",
@@ -87,16 +86,16 @@ df = df.rename(columns={"FFA Number": "id",
 
 # Big St Peters 2022 fix
 
-df.loc[df['id'] == 10192438, 'team'] = 'St Peters B' #Angel Lopez
-df.loc[df['id'] == 10192353, 'team'] = 'St Peters B' #Katianos, John
-df.loc[df['id'] == 80860398, 'team'] = 'St Peters B' #Burley, Richard J
-df.loc[df['id'] == 79697306, 'team'] = 'St Peters B' #Mileto, Domenico
-df.loc[df['id'] == 11447695, 'team'] = 'St Peters B' #Martelotto, Luciano
-df.loc[df['id'] == 11450229, 'team'] = 'St Peters B' #Scholl, Ivan
-df.loc[df['id'] == 11276224, 'team'] = 'St Peters B' #Russell, Martin
-df.loc[df['id'] == 11449192, 'team'] = 'St Peters B' #Sikora, Grzegorz
-df.loc[df['id'] == 11450666, 'team'] = 'St Peters B' #Guzman, Jerson
-df.loc[df['id'] == 11470835, 'team'] = 'St Peters B' #Lugo, Alex
+# df.loc[df['id'] == 10192438, 'team'] = 'St Peters B' #Angel Lopez
+# df.loc[df['id'] == 10192353, 'team'] = 'St Peters B' #Katianos, John
+# df.loc[df['id'] == 80860398, 'team'] = 'St Peters B' #Burley, Richard J
+# df.loc[df['id'] == 79697306, 'team'] = 'St Peters B' #Mileto, Domenico
+# df.loc[df['id'] == 11447695, 'team'] = 'St Peters B' #Martelotto, Luciano
+# df.loc[df['id'] == 11450229, 'team'] = 'St Peters B' #Scholl, Ivan
+# df.loc[df['id'] == 11276224, 'team'] = 'St Peters B' #Russell, Martin
+# df.loc[df['id'] == 11449192, 'team'] = 'St Peters B' #Sikora, Grzegorz
+# df.loc[df['id'] == 11450666, 'team'] = 'St Peters B' #Guzman, Jerson
+# df.loc[df['id'] == 11470835, 'team'] = 'St Peters B' #Lugo, Alex
 
 #To register - Edgar Costa
 #To register - Oscar ??
--- src/Login.js
+++ src/Login.js
@@ -65,7 +65,7 @@ const Login= (props) => {
                       </Col>
                   </Form.Group>
               </Form>
-              <p><i>Version 1.5.1 (01/04/24)</i></p>
+              <p><i>Version 1.5.2 (04/04/24)</i></p>
           </Jumbotron>
 
               {/*<button id="fetch-btn">Fetch</button>*/}
